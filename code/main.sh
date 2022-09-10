3b84316e06c3212724fa489de6d6334561281910
shadzic <shadzic@users.noreply.github.com>
2022-09-10 17:03:26 +0200

    correct extension file

--- code/main.sh
+++ code/main.sh
@@ -11,4 +11,4 @@ python deduplicate_data.py
 scp -r ls_test@116.203.20.255:/data/clean_data coolify@116.203.20.255:/data/clean_data
 
 # Append clean data to Coolify Database
-python append_coolify_db.python
+python append_coolify_db.pythonpython append_coolify_db.py
