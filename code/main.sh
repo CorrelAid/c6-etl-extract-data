9ef2f2a76a619dfb418209fd4e323e090549a102
shadzic <shadzic@users.noreply.github.com>
2022-09-10 17:10:06 +0200

    copy from local files

--- code/main.sh
+++ code/main.sh
@@ -2,13 +2,13 @@
 python get_raw_data.py
 
 # Save the raw data from limesurvey server to coolify server as a backup
-scp -r ls_test@116.203.20.255:/data/raw_data coolify@116.203.20.255:/data/raw_data
+scp -r /data/raw_data coolify@116.203.20.255:/data/raw_data
 
 # Deduplicate the data
 python deduplicate_data.py
 
 # Save the clean data from limesurvey server to coolify server as a backup
-scp -r ls_test@116.203.20.255:/data/clean_data coolify@116.203.20.255:/data/clean_data
+scp -r /data/clean_data coolify@116.203.20.255:/data/clean_data
 
 # Append clean data to Coolify Database
 python append_coolify_db.pythonpython append_coolify_db.py
